850bc3adc69e290b01dcbbf7b70df90da306186b
Brian Rinaldi <brian.rinaldi@gmail.com>
2019-05-16 20:19:31 -0400

    simplistic search working

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+var yaml = require("yaml");
+var S = require("string");
+
+var CONTENT_PATH_PREFIX = "content";
+
+module.exports = function(grunt) {
+
+    grunt.registerTask("lunr-index", function() {
+
+        grunt.log.writeln("Build pages index");
+
+        var indexPages = function() {
+            var pagesIndex = [],
+                processedContent;
+            grunt.file.recurse(CONTENT_PATH_PREFIX, function(abspath, rootdir, subdir, filename) {
+                grunt.verbose.writeln("Parse file:",abspath);
+                processedContent = processFile(abspath, filename);
+                if (processedContent) {
+                    pagesIndex.push(processedContent);
+                }
+            });
+
+            return pagesIndex;
+        };
+
+        var processFile = function(abspath, filename) {
+            var pageIndex;
+
+            if (S(filename).endsWith(".html")) {
+                pageIndex = processHTMLFile(abspath, filename);
+            } else {
+                pageIndex = processMDFile(abspath, filename);
+            }
+
+            return pageIndex;
+        };
+
+        var processHTMLFile = function(abspath, filename) {
+            var content = grunt.file.read(abspath);
+            var pageName = S(filename).chompRight(".html").s;
+            var href = S(abspath)
+                .chompLeft(CONTENT_PATH_PREFIX).s;
+            return {
+                title: pageName,
+                href: href,
+                content: S(content).trim().stripTags().stripPunctuation().s
+            };
+        };
+
+        var processMDFile = function(abspath, filename) {
+            var content = grunt.file.read(abspath);
+            var pageIndex;
+            // First separate the Front Matter from the content and parse it
+            content = content.split("---");
+            var frontMatter;
+            try {
+                frontMatter = yaml.parse(content[1].trim());
+            } catch (e) {
+                console.log(e.message);
+            }
+
+            var href = S(abspath).chompLeft(CONTENT_PATH_PREFIX).chompRight(".md").s;
+            // href for index.md files stops at the folder name
+            if (filename === "index.md") {
+                href = S(abspath).chompLeft(CONTENT_PATH_PREFIX).chompRight(filename).s;
+            }
+
+            // Build Lunr index for this page
+            if (frontMatter) {
+                pageIndex = {
+                    title: frontMatter.title,
+                    tags: frontMatter.tags,
+                    href: href
+                };
+                if (content) {
+                    pageIndex.content = S(content[2]).trim().stripTags().stripPunctuation().s;
+                }
+            }
+
+            return pageIndex;
+        };
+
+        grunt.file.write("static/js/lunr/PagesIndex.json", JSON.stringify(indexPages()));
+        grunt.log.ok("Index built");
+    });
+};
