1b1372b256b307f283371f52b6a99c5f41af814e
Michael Trythall <m@mtrythall.com>
2020-05-23 01:02:18 -0400

    Initial redesign integration

---
+++ README.md
@@ -0,0 +1,12 @@
+# Certified Fresh Events
+
+## Installation
+
+Install Hugo: `brew install hugo`
+
+Make sure to install `postcss-cli` and `autoprefixer` globally in your environment, as Hugo Pipe’s PostCSS requires it. This is mentioned in the [Hugo Docs](https://gohugo.io/hugo-pipes/postcss/).
+
+```bash
+npm install -g postcss-cli
+npm install -g autoprefixer
+```
---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      
+      black      : '#040419',
+      white      : '#FFFFFF',
+
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+        600: '#718096',
+        700: '#4a5568',
+        800: '#2d3748',
+        900: '#1a202c',
+      },
+
+      sky        : '#29AAE2',
+      blue       : '#0D3B66',
+      lightBlue  : '#BAEBF4',
+      jelly      : '#247BA0',
+
+      sand       : '#FFFFF0',
+      sunset     : '#F7AE70',
+      salmon     : '#FF8360',
+
+      java       : '#1D958D',
+      carlo      : '#73BFB8',
+      flower     : '#B9E3C6',
+
+      red        : '#E53E3E',
+
+      facebook   : '#39599F',
+      twitter    : '#55ACEE',
+      youtube    : '#FF0000',
+      reddit     : '#FF5700',
+      instagram  : '#405DE6',
+      rss        : '#336699'
+    },
+  },
+  variants: {},
+  plugins: [],
+}
