b82645173040a132b977fabbccc6e08675337a83
Michael Trythall <m@mtrythall.com>
2023-09-01 15:14:18 -0400

    Adds improved search view. Fixes various issues with navigation.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,27 +1,32 @@
 function getQueryVariable(variable)
 {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
+  let query = window.location.search.substring(1);
+  let vars = query.split("&");
+  for (let i=0;i<vars.length;i++) {
+    let pair = vars[i].split("=");
     if(pair[0] == variable){return pair[1];}
   }
   return(false);
 }
 
-const query = getQueryVariable('search')
+const query = getQueryVariable('search');
 if (query) {
   const loader = document.getElementById('loader');
   const searchResults = document.getElementById('searchResults');
   const searchDescription = document.getElementById('searchDescription');
+  const searchPageInput = document.getElementById('searchPageInput');
   axios.post('/.netlify/functions/algolia', {
     search: query
   })
     .then(function (response) {
       loader.hidden = true;
-      if (response.data == '')
-        response.data = '<div style="text-align:left;">You\'re search didn\'t return any results</div>';
-      searchResults.innerHTML = response.data;
-      searchDescription.innerHTML = 'Search results for: "' + query + '"';
+      const decodedQuery = decodeURIComponent(query);
+      searchPageInput.value = decodedQuery
+      searchDescription.innerHTML = `${response.data.length} results for "${decodedQuery}"`
+      if (response.data == '') {
+        searchResults.innerHTML = '<h2 class="text-center font-bold text-lg mb-12">You\'re search didn\'t return any results</h2>';
+      } else {
+        searchResults.innerHTML = `<div class="grid grid-cols-1 gap-6  md:grid-cols-2 lg:gap-8">${response.data}</div>`;
+      }
     });
 }
--- content/search.md
+++ content/search.md
@@ -3,4 +3,11 @@ title: "Search"
 date: 2017-07-12T18:52:51-04:00
 layout: search
 draft: false
+
+featuredTerms:
+- headless cms
+- edge computing
+- GraphQL
+- generative AI
+- open source
 ---
