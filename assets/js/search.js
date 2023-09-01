0ab6aa279d4d5b4aca902f0cce3dcef80fe2b58d
Brian Rinaldi <brian.rinaldi@gmail.com>
2023-09-01 17:25:07 -0400

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,30 +1,33 @@
-function getQueryVariable(variable)
-{
+function getQueryVariable(variable) {
   let query = window.location.search.substring(1);
   let vars = query.split("&");
-  for (let i=0;i<vars.length;i++) {
+  for (let i = 0; i < vars.length; i++) {
     let pair = vars[i].split("=");
-    if(pair[0] == variable){return pair[1];}
+    if (pair[0] == variable) {
+      return pair[1];
+    }
   }
-  return(false);
+  return false;
 }
 
-const query = getQueryVariable('search');
+const query = getQueryVariable("search");
 if (query) {
-  const loader = document.getElementById('loader');
-  const searchResults = document.getElementById('searchResults');
-  const searchDescription = document.getElementById('searchDescription');
-  const searchPageInput = document.getElementById('searchPageInput');
-  axios.post('/.netlify/functions/algolia', {
-    search: query
-  })
+  const loader = document.getElementById("loader");
+  const searchResults = document.getElementById("searchResults");
+  const searchDescription = document.getElementById("searchDescription");
+  const searchPageInput = document.getElementById("searchPageInput");
+  axios
+    .post("/.netlify/functions/algolia", {
+      search: decodeURIComponent(query),
+    })
     .then(function (response) {
       loader.hidden = true;
-      const decodedQuery = decodeURIComponent(query);
-      searchPageInput.value = decodedQuery
-      searchDescription.innerHTML = `${response.data.length} results for "${decodedQuery}"`
-      if (response.data == '') {
-        searchResults.innerHTML = '<h2 class="text-center font-bold text-lg mb-12">You\'re search didn\'t return any results</h2>';
+      const decodedQuery = decodeURIComponent(query).replace(/\+/g, " ");
+      searchPageInput.value = decodedQuery;
+      searchDescription.innerHTML = `${response.data.length} results for "${decodedQuery}"`;
+      if (response.data == "") {
+        searchResults.innerHTML =
+          "<h2 class=\"text-center font-bold text-lg mb-12\">You're search didn't return any results</h2>";
       } else {
         searchResults.innerHTML = `<div class="grid grid-cols-1 gap-6  md:grid-cols-2 lg:gap-8">${response.data}</div>`;
       }
