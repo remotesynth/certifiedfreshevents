6c9f3cf6df6ae04a34aec1111745cdf36413a1f1
Brian Rinaldi <brian.rinaldi@gmail.com>
2024-01-29 07:49:54 -0500

    fix search

--- netlify/functions/algolia/index.mjs
+++ netlify/functions/algolia/index.mjs
@@ -1,21 +1,21 @@
 import algoliasearch from "algoliasearch";
 
 export default async (req, context, callback) => {
-    try {
-        const body = await req.body;
-        const { search } = body;
-        const client = algoliasearch(
-            process.env.ALGOLIA_APP_ID,
-            process.env.ALGOLIA_ADMIN_KEY
-        );
-        const index = client.initIndex("Pages");
-        let searchResultsHTML = "";
+  try {
+    const body = await req.json();
+    const { search } = body;
+    const client = algoliasearch(
+      process.env.ALGOLIA_APP_ID,
+      process.env.ALGOLIA_ADMIN_KEY
+    );
+    const index = client.initIndex("Pages");
+    let searchResultsHTML = "";
 
-        await index
-            .search(search)
-            .then(({ hits }) => {
-                hits.map((hit) => {
-                    let itemHTML = `
+    await index
+      .search(search)
+      .then(({ hits }) => {
+        hits.map((hit) => {
+          let itemHTML = `
         <article class="event-card">
           <img
             src="/img/banners/${hit.banner}"
@@ -43,24 +43,24 @@ export default async (req, context, callback) => {
           </div>
         </article>
         `;
-                    searchResultsHTML += itemHTML;
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        return new Response(searchResultsHTML, {
-            status: 200,
-            headers: {
-                "Content-Type": "text/html",
-            },
+          searchResultsHTML += itemHTML;
         });
-    } catch (err) {
-        [];
-        return new Response(err.toString(), { status: 500 });
-    }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return new Response(searchResultsHTML, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  } catch (err) {
+    [];
+    return new Response(err.toString(), { status: 500 });
+  }
 };
 
 export const config = {
-    path: "/api/algolia",
+  path: "/api/algolia",
 };
